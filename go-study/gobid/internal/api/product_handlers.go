2bba378e7295bc5517e72818865dc39ff997f2ad
Gabriel Silva <devzgabriel@gmail.com>
2025-01-01 16:46:44 -0300

    feat: initialize gobid project with database configuration, migrations, and API structure

---
+++ go-study/gobid/internal/api/product_handlers.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/rocketseat-education/gobid/internal/jsonutils"
+	"github.com/rocketseat-education/gobid/internal/services"
+	"github.com/rocketseat-education/gobid/internal/usecase/product"
+)
+
+func (api *Api) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	data, problems, err := jsonutils.DecodeValidJson[product.CreateProductReq](r)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	userID, ok := api.Sessions.Get(r.Context(), "AuthenticatedUserId").(uuid.UUID)
+	if !ok {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "unexpected error, try again later",
+		})
+		return
+	}
+
+	productId, err := api.ProductService.CreateProduct(
+		r.Context(),
+		userID,
+		data.ProductName,
+		data.Description,
+		data.Baseprice,
+		data.AuctionEnd,
+	)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "failed to create product auction try again later",
+		})
+		return
+	}
+
+	ctx, _ := context.WithDeadline(context.Background(), data.AuctionEnd)
+	auctionRoom := services.NewAuctionRoom(ctx, productId, api.BidsService)
+
+	go auctionRoom.Run()
+
+	api.AuctionLobby.Lock()
+	api.AuctionLobby.Rooms[productId] = auctionRoom
+	api.AuctionLobby.Unlock()
+
+	jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"message":    "Auction has started with success",
+		"product_id": productId,
+	})
+}
---
+++ go-study/gobid/readme.md
@@ -0,0 +1,15 @@
+# Projeto de leilão
+
+## Building to learn Go with
+- Websockets
+- Sessions
+- Password hashing
+- Security and CSRF
+- Project Structure
+- Live Reloading
+
+## Project Structure
+
+![Arch Design](image.png)
+
+From: https://github.com/rocketseat-education/gobid
